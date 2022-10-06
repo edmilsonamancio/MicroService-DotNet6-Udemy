f93f9a292b76ceca37903985fe961723417e4e6a
Edmilson Amancio <edmilson.aamancio@gmail.com>
2022-10-06 08:31:27 -0300

    73. 0806 Iniciando a Implementação do Repositório de Produtos

--- 507_Microservices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ 507_Microservices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -2,10 +2,10 @@
 
 namespace GeekShopping.ProductAPI.Repository
 {
-    public class IProductRepository
+    public interface IProductRepository
     {
         Task<IEnumerable<ProductVO>> FindAll();
-        Task<ProductVO> FindById(longId);
+        Task<ProductVO> FindById(long id);
         Task<ProductVO> Create(ProductVO productVO);
         Task<ProductVO> Update(ProductVO productVO);
         Task<bool> Delete(long id);
