0157379709a95f54a21a4859a9de3a8e510fd8f0
Edmilson Amancio <edmilson.aamancio@gmail.com>
2022-10-05 22:26:44 -0300

    Commit Inicial

---
+++ 507_Microservices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,4 @@
+using GeekShopping.ProductAPI;
+
+var builder = WebApplication.CreateBuilder(args)
+        .UseStartup<Startup>();
---
+++ 507_Microservices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Startup.cs
@@ -0,0 +1,63 @@
+﻿using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI
+{
+    public class Startup : IStartup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"]; 
+
+            services.AddDbContext<MySQLContext>(options => options
+                .UseMySql(connection,
+                    new MySqlServerVersion(
+                        new Version(5, 6))));
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+        }
+    }
+
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+
+        void Configure(WebApplication app, IWebHostEnvironment environment);
+        void ConfigureServices(IServiceCollection services);
+    }
+
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder WebAppBuilder) where TStartup : IStartup
+        {
+            var startup = Activator.CreateInstance(typeof(TStartup), WebAppBuilder.Configuration) as IStartup;
+            if (startup == null)
+                throw new ArgumentException("Classe Startup.cs inválida!");
+
+            startup.ConfigureServices(WebAppBuilder.Services);
+
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
+        }
+    }
+}
