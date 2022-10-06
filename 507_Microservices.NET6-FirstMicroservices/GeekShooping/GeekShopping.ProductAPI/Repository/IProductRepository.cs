fb7c03942256e857e20add382b4d7a9876b89436
Edmilson Amancio <edmilson.aamancio@gmail.com>
2022-10-05 22:34:12 -0300

    71. 0804 Definindo as Propriedades da Interface IProductRepository

--- 507_Microservices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ 507_Microservices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace GeekShopping.ProductAPI.Repository
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
 {
     public class IProductRepository
     {
-
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(longId);
+        Task<ProductVO> Create(ProductVO productVO);
+        Task<ProductVO> Update(ProductVO productVO);
+        Task<bool> Delete(long id);
     }
 }
